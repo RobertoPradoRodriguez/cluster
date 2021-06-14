4236703d0708cd99c7df897162b3e1390c01c077
roberto <roberto.prado@udc.es>
2021-06-15 00:21:45 +0200

    Changes in slurm (FW, Obtain mem) and Init.yml (Firewall)

--- synced_folder/mpi-jobs/script.sh
+++ synced_folder/mpi-jobs/script.sh
@@ -13,21 +13,15 @@
 
 module purge
 module load gnu9
-module load mpich/3.3.2-ofi
+module load mpich
 
 mpicc hello-mpi.c -o hello-mpi -Wall -Wextra
-#export LD_LIBRARY_PATH=/usr/local/lib
-#mpirun -np 2 -x LD_LIBRARY_PATH ./hello-mpi
-#PATH=$PATH:/opt/ohpc/pub/mpi/openmpi4-gnu9/4.0.5/bin
-#LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/ohpc/pub/mpi/openmpi4-gnu9/4.0.5/lib
 #mpirun -np 2 --mca btl_tcp_if_include eth1 -mca plm rsh --hostfile /synced_folder/mpi-jobs/hostfile ./hello-mpi
 
-#mpirun -np 2 --debugger --mca btl_base_verbose 30 ./hello-mpi
-
+#MPICH AND MVAPICH2
 mpirun -np $SLURM_NTASKS -iface eth1 ./hello-mpi
 
-#echo SLURM_JOB_NODELIST=$SLURM_JOB_NODELIST
-#echo SLURM_NTASKS=$SLURM_NTASKS
-#echo SLURM_JOB_NUM_NODES=$SLURM_JOB_NUM_NODES
+#OPENMPI
+#mpirun -np $SLURM_NTASKS ./hello-mpi
 
-#srun --mpi=pmi2 ./hello-mpiI_MPI_OFI_PROVIDER=tcp
+#srun --mpi=pmi2 ./hello-mpi
